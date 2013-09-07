9cfa2ccbb826f10ccc723b0a04d356ff754c6469
Etienne Ad <aze>
2013-09-06 21:21:05 -0400

    getting there!

--- src/ios/ProgressHud.h
+++ src/ios/ProgressHud.h
@@ -5,10 +5,10 @@
 //
 // Copyright 2011 Olivier Louvignes. All rights reserved.
 // MIT Licensed
-//
 
 #import <Foundation/Foundation.h>
 #import <Cordova/CDVPlugin.h>
+#import "MBProgressHUD.h"
 
 @interface ProgressHud : CDVPlugin {
 
